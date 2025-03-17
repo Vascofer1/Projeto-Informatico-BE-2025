b84ecbb8d6bf4e1ee49c05f6fda0bd4f0df7bd2e
Vasco Fernandes <2221392@my.ipleiria.pt>
2025-03-17 20:19:23 +0000

    commit configuraçoes

---
+++ PI-back-end/resources/js/pages/Events.vue
@@ -0,0 +1,9 @@
+
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <h1>Oláaaasas!</h1>
+</template>
--- PI-back-end/routes/web.php
+++ PI-back-end/routes/web.php
@@ -2,14 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\EventController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return Inertia::render('Welcome');
-})->name('home');
+})->name('home');*/
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/events', function () {
+    return Inertia::render('Events');
+})->name('events');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
