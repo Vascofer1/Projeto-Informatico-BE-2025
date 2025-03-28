8e1ea16ece772745c2cbd89cd2f6a8ddb14258d1
Vasco Fernandes <2221392@my.ipleiria.pt>
2025-03-28 13:49:54 +0000

    exportar participantes e geraçao de qrcode

--- PI-back-end/routes/web.php
+++ PI-back-end/routes/web.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\ParticipantController;
+use App\Exports\ParticipantsExport;
+use Maatwebsite\Excel\Facades\Excel;
+
+use App\Mail\ParticipantRegistered;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Participant;
+use App\Models\Event;
 
 /*Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -26,6 +33,31 @@ Route::get('/inscricao/{id}', [EventController::class, 'showRegistrationPage'])-
 Route::post('/inscricao/{id}', [ParticipantController::class, 'store'])->name('participants.store');
 
 Route::get('/event/{id}/participants', [ParticipantController::class, 'index']);
+
+Route::get('/eventos/{event}/participantes/export-csv', [ParticipantController::class, 'export']);
+
+Route::get('/eventos/{event}/participantes/export-pdf', [ParticipantController::class, 'exportpdf']);
+
+Route::get('/test-email', function () {
+    // Simulação do evento
+    $event = new Event([
+        'name' => 'Laravel Workshop',
+        'location' => 'Lisboa',
+        'date' => '2025-04-15',
+    ]);
+
+    // Simulação do participante e associação manual ao evento
+    $participant = new Participant([
+        'name' => 'John Doe',
+        'email' => 'johndoe@example.com',
+    ]);
+    $participant->event = $event; // adicionamos o evento manualmente
+
+    // Enviar o email
+    Mail::to($participant->email)->send(new ParticipantRegistered($participant));
+
+    return 'Email enviado com sucesso!';
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
